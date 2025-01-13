da3c2ddd77f044515d1b4ab070756fda03257588
Shizuo Fujita <fujita@clear-code.com>
2025-01-13 12:33:25 +0900

    Install proper version of bundler
    Signed-off-by: Shizuo Fujita <fujita@clear-code.com>

--- ci/apt-test.sh
+++ ci/apt-test.sh
@@ -10,6 +10,6 @@ apt install -V -y lsb-release
 apt install -V -y ruby-dev git build-essential pkg-config
 apt install -V -y libcap-ng-dev
 cd /capng && \
-    gem install bundler --no-document && \
+    gem install bundler -v 2.3.27 --no-document && \
     bundle install && \
     bundle exec rake
--- ci/yum-test.sh
+++ ci/yum-test.sh
@@ -60,5 +60,5 @@ if [ $USE_SCL -eq 1 ]; then
     export MANPATH=
     cd /capng && source /opt/rh/rh-ruby26/enable && gem install bundler --no-document && bundle install && bundle exec rake
 else
-    cd /capng && gem install bundler --no-document && bundle install && bundle exec rake
+    cd /capng && gem install bundler -v 2.3.27 --no-document && bundle install && bundle exec rake
 fi
